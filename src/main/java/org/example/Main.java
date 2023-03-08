82f7157550a97dd778f8262120391d998e3810c2
Nathan <naccaratonathan@gmail.com>
2023-03-08 09:34:36 -0500

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,18 @@ public class Main {
         System.out.println(river.length());
         river = river.replace("ss","s");
         System.out.println(river.length());
+
+        // 2.6
+
+        java.awt.Rectangle box = new java.awt.Rectangle(5, 10, 20, 30);
+        System.out.println(box);
+        //java.awt.Rectangle[x=5,y=10,width=20,height=30]
+
+        box.add(0, 0);
+        System.out.println(box);
+        //java.awt.Rectangle[x=5,y=10,width=25,height=40]
+
+
     }
 
 }
