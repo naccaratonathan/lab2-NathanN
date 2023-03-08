b663a789bf8b81e0190f09240655a087e6862482
nacc2221 <naccaratonathan@gmail.com>
2023-03-08 12:27:03 -0500

    commit

---
+++ src/main/java/org/example/PerimeterTester.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.awt.*;
+
+public class PerimeterTester {
+
+    public static void main(String[] args){
+
+        Rectangle rec = new Rectangle(45, 60);
+        double perimeter = 2*(rec.getWidth()) + 2*(rec.getHeight());
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Expected: (210.0)");
+
+
+    }
+}
